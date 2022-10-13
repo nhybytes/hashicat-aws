bc2cfdf35d2c2b2f63867c581adf121e9bba54ef
hieu <nghiem.hieu@hybytes.com>
2022-10-13 16:41:28 +0700

    Added core-infra for ECS

--- variables.tf
+++ variables.tf
@@ -48,3 +48,33 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "core_stack_name" {
+  description = "The name of Core Infrastructure stack, feel free to rename it. Used for cluster and VPC names."
+  type        = string
+  default     = "ecs-blueprint-infra"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "namespaces" {
+  description = "List of service discovery namespaces for ECS services. Creates a default namespace"
+  type        = list(string)
+  default     = ["default", "myapp"]
+}
+
+variable "enable_nat_gw" {
+  description = "Provision a NAT Gateway in the VPC"
+  type        = bool
+  default     = true
+
+}
